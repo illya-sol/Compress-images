689300f13871bccc0c4c8708879b5a6c76993e03
Illya Solodky <xcrz@Illyas-MBP.lan1>
2021-12-07 10:02:00 -0800

    initial

---
+++ index.js
@@ -0,0 +1,18 @@
+const compress_images = require('compress-images')
+
+const INPUT = './samples/**/*.{jpg,JPG,jpeg,JPEG,png}';
+const OUTPUT = 'samples comp/';
+
+compress_images(INPUT, OUTPUT, { compress_force: true, statistic: true, autoupdate: true }, false,
+  { jpg: { engine: "mozjpeg", command: ["-quality", "60"] } },
+  { png: { engine: "pngquant", command: ["--quality=20-50", "-o"] } },
+  { svg: { engine: "svgo", command: "--multipass" } },
+  { gif: { engine: "gifsicle", command: ["--colors", "64", "--use-col=web"] } },
+  function (error, completed, statistic) {
+    console.log("-------------");
+    console.log(error);
+    console.log(completed);
+    console.log(statistic);
+    console.log("-------------");
+  }
+);
